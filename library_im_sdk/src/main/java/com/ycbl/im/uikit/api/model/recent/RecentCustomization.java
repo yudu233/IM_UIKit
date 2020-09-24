3182f440a817f0e6eb3063d24a1f58d40e054d35
yudu233 <yudu233@gmail.com>
2020-09-24 15:40:54 +0800

    🌵 会话项默认文案定制

---
+++ library_im_sdk/src/main/java/com/ycbl/im/uikit/api/model/recent/RecentCustomization.java
@@ -0,0 +1,17 @@
+package com.ycbl.im.uikit.api.model.recent;
+
+import com.netease.nimlib.sdk.msg.model.RecentContact;
+
+import java.io.Serializable;
+
+/**
+ * @Author : Rain
+ * @CreateDate : 2020/9/24 10:15
+ * @Version : 1.0
+ * @Descroption : 会话项默认文案定制 
+ */
+public class RecentCustomization<T> implements Serializable {
+    public String getDefaultDigest(RecentContact recent) {
+        return "";
+    }
+}
